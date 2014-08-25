bb524be78347eafc4aa2c29953de36ea0e8b6a6e
Matthew <github@arcus.globalnet.co.uk>
2014-08-25 16:36:34 +0100

    Checkpoint

--- utils.h
+++ utils.h
@@ -10,6 +10,13 @@ T min3(T t1, T t2, T t3) {
   return std::min(t1,std::min(t2,t3));
 }
 template <typename T>
+T mid3(T t1, T t2, T t3) {
+  if (t1 > t2) std::swap(t1,t2);
+  if (t2 > t3) std::swap(t2,t3);
+  if (t1 > t2) std::swap(t1,t2);
+  return t2;
+}
+template <typename T>
 T square(T t) { return t * t; }
 
 std::ostream &operator<<(std::ostream &str, const glm::vec3 &a)
@@ -21,12 +28,15 @@ std::ostream &operator<<(std::ostream &str, const glm::vec3 &a)
 glm::vec4 black( 0, 0, 0, 1 );
 glm::vec4 white( 1, 1, 1, 1 );
 glm::vec4 red( 1, 0, 0, 1 );
+glm::vec4 pink( 1, 0.5, 0.5, 1 );
+glm::vec4 grey( 0.5, 0.5, 0.5, 1 );
 glm::vec4 orange( 1, 0.5, 0, 1 );
 glm::vec4 green( 0, 1, 0, 1 );
 glm::vec4 blue( 0, 0, 1, 1 );
 glm::vec4 yellow( 1, 1, 0, 1 );
 glm::vec4 cyan( 0, 1, 1, 1 );
 glm::vec4 magenta( 1, 0, 1, 1 );
+glm::vec4 purple( 1, 0.5, 1, 1 );
 glm::vec4 mist( 1, 1, 1, 0.3 );
 
 // Generate series of values in a range.
